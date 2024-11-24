24c3b741e08bc93bbc6454b52c9439828ed6beab
Gergő Csókás <gergo.csokas@gmail.com>
2024-11-24 11:45:29 +0700

    :art: Introducing `FileManager` struct for better filehandling and some proper error handling

--- filemanager/filemanager.go
+++ filemanager/filemanager.go
@@ -7,8 +7,20 @@ import (
 	"os"
 )
 
-func ReadLines(path string) ([]string, error) {
-	fp, err := os.Open(path)
+type FileManager struct {
+	InputFilePath  string
+	OutputFilePath string
+}
+
+func New(inputFilePath, outputFilePath string) FileManager {
+	return FileManager{
+		inputFilePath,
+		outputFilePath,
+	}
+}
+
+func (fm FileManager) ReadLines() ([]string, error) {
+	fp, err := os.Open(fm.InputFilePath)
 
 	if err != nil {
 		return nil, errors.New("an error occurred while opening prices.txt")
@@ -25,17 +37,23 @@ func ReadLines(path string) ([]string, error) {
 	err = scanner.Err()
 
 	if err != nil {
-		fp.Close()
+		err := fp.Close()
+		if err != nil {
+			return nil, err
+		}
 		return nil, errors.New("an error occurred while reading prices.txt")
 	}
 
-	fp.Close()
+	err = fp.Close()
+	if err != nil {
+		return nil, err
+	}
 
 	return lines, nil
 }
 
-func WriteJSON(path string, data any) error {
-	fp, err := os.Create(path)
+func (fm FileManager) WriteJSON(data any) error {
+	fp, err := os.Create(fm.OutputFilePath)
 
 	if err != nil {
 		return errors.New("failed to create file")
@@ -44,10 +62,16 @@ func WriteJSON(path string, data any) error {
 	err = json.NewEncoder(fp).Encode(data)
 
 	if err != nil {
-		fp.Close()
+		err := fp.Close()
+		if err != nil {
+			return err
+		}
 		return errors.New("failed to encode file")
 	}
 
-	fp.Close()
+	err = fp.Close()
+	if err != nil {
+		return err
+	}
 	return nil
 }
